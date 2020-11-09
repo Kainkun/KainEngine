9f3b626deb0af29609d168b74c84a78f32f8b2e8
Kain Shepard <kainkun100@gmail.com>
2020-11-09 01:17:22 -0800

    Triangle!

--- KainEngine/src/KainEngine/Platform/OpenGL/OpenGLContext.cpp
+++ KainEngine/src/KainEngine/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace KainEngine
 		glfwMakeContextCurrent(m_windowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		KE_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		KE_CORE_INFO("OpenGL Info:");
+		KE_CORE_INFO("  Vendor:   {0}", glGetString(GL_VENDOR));
+		KE_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		KE_CORE_INFO("  Version:  {0}", glGetString(GL_VERSION));
 	}
 	void OpenGLContext::SwapBuffers()
 	{
